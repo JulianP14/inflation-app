0ebfd0a736fdab5ee0ebcddab90a39895ebceced
julian parera <julian.parera@hotmail.com>
2023-08-15 20:33:56 -0300

    Probando cambiar estilos

--- Components/InputAmountNow.jsx
+++ Components/InputAmountNow.jsx
@@ -6,14 +6,14 @@ const InputAmountNow = ({ onChange }) => {
             <label className='flex flex-col my-2'>
                 <div className='flex flex-row'>
                     <span className='mr-2'>
-                        Ingresa el precio de contadoAA
+                        Ingresa el precio de contado
                     </span>
                     <span>💰</span>
                 </div>
             </label>
                 <input
                     type='number'
-                    className='border-b-2 border-white rounded-lg p-1 px-2 bg-transparent hover:border-b-cyan-400 duration-300 outline-none text-white'
+                    className=''
                     onBlur={onChange}
 
                 />
