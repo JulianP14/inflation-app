2a142f98bf18d25239f456b76c13bb8f31b8daf9
julian parera <julian.parera@hotmail.com>
2023-08-15 20:27:54 -0300

    Probando cambios

--- Components/InputAmountNow.jsx
+++ Components/InputAmountNow.jsx
@@ -10,13 +10,13 @@ const InputAmountNow = ({ onChange }) => {
                     </span>
                     <span>💰</span>
                 </div>
+            </label>g
                 <input
                     type='number'
                     className='border-b-2 border-white rounded-lg p-1 px-2 bg-transparent hover:border-b-cyan-400 duration-300 outline-none text-white'
                     onBlur={onChange}
 
                 />
-            </label>
         </div>
     )
 };
