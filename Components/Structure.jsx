1c66d9670ca1bf161e9447d0343239d00b2f1574
julian parera <julian.parera@hotmail.com>
2023-12-15 14:30:51 -0300

    Cambio/ocultamiento de componente innecesario

--- Components/Structure.jsx
+++ Components/Structure.jsx
@@ -9,7 +9,7 @@ const Structure = () => {
         <>
             <div className='flex flex-col px-4 pt-10'>
                 <MainTitle />
-                <Donations />
+                {/* <Donations /> */}
             </div>
             <Inputs />
             <Footer />
